16b0bd2a76e3abb668204ef7e88980ab21f331d2
jcameron10 <jovonnec@gmail.com>
2021-06-22 13:46:57 -0400

    infinite scroll

--- infinitescroll/src/App.js
+++ infinitescroll/src/App.js
@@ -1,46 +1,44 @@
 import "./App.css";
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import Board from "./components/Board";
 import { pinData } from "./data";
 import axios from "axios";
-import InfiniteScroll from 'react-infinite-scroll-component'
+import InfiniteScroll from "react-infinite-scroll-component";
 
 const pageNumber = 1;
-function App() {
+function App(props) {
   const [state, setState] = useState([]);
   const [page, setPage] = useState(pageNumber);
-
-
+  // const pinData = props
+  console.log(pinData)
   useEffect(() => {
     axios(pinData)
-    .then(res=>res.data)
-    .then(json =>setState([...state,...json.data]))
-    console.log("it works")
-  }, [page])
-
-  // const onScrollToEnd = () => {
-  //   setPage(page + 1)
-  // }
-  // window.onscroll = function () {
-  //   if (
-  //     window.innerheight + document.documentElement.scrollTop
-  //     >= document.documentElement.offsetHeight
-  //   ) {
-  //     onScrollToEnd();
-  //   }
-  // }
-
+      .then((res) => res.data)
+      .then((json) => setState([...state, ...json.data]));
+  }, [page]);
   return (
     <div className="App">
       <InfiniteScroll>
-      dataLength={state.length} 
-      next={()=>setPage(page+1)}
-      hasMore={true}
-      <Board Board={Board} />
-  
-    </InfiniteScroll>
+        dataLength={pinData.length}
+        next={() => setPage(page + 1)}
+        hasMore={true}
+        <Board Board={Board} />
+        {pinData}
+      </InfiniteScroll>
     </div>
   );
 }
 
 export default App;
+
+// const onScrollToEnd = () => {
+//   setPage(page + 1)
+// }
+// window.onscroll = function () {
+//   if (
+//     window.innerheight + document.documentElement.scrollTop
+//     >= document.documentElement.offsetHeight
+//   ) {
+//     onScrollToEnd();
+//   }
+// }
--- infinitescroll/src/components/Board.js
+++ infinitescroll/src/components/Board.js
@@ -1,39 +1,21 @@
-import { pinData } from "../data"
+import { pinData } from "../data";
 
 export default function Board() {
-  // const{data} =props
-  const pinDataArray =[pinData]
-    // Object.entries(pinData)
-  // console.log(pinDataArray)
   const imgJsx = pinData.map((data, key) => {
-    return(
+    return (
       <div className="pin-card" key={key}>
         <h4 className="username">{data.pinner.username}</h4>
-        <img src={data.images["136x136"].url} />
-        <p>{ data.description}</p>
+        <img src={data.images["236x"].url} />
+        <p>{data.description}</p>
         <p>{data.like_count}</p>
       </div>
-  )})
-  
+    );
+  });
+
   return (
     <div className="board">
       <h1>Pinterest</h1>
       {imgJsx}
-            
-
-
-      {/* {pinData.map((data, key) => {
-        return (
-          <div key={key}>
-            <div>{data.images}</div>
-          </div>
-        )
-      })} */}
-      
     </div>
-  )
+  );
 }
-
-
-
-
