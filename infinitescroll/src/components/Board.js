8eb0552fe093afa9bb59561227ef77c74c0907c5
jcameron10 <jovonnec@gmail.com>
2021-06-21 21:33:22 -0400

    created react app

---
+++ infinitescroll/src/components/Board.js
@@ -0,0 +1,39 @@
+import { pinData } from "../data"
+
+export default function Board() {
+  // const{data} =props
+  const pinDataArray =[pinData]
+    // Object.entries(pinData)
+  // console.log(pinDataArray)
+  const imgJsx = pinData.map((data, key) => {
+    return(
+      <div className="pin-card" key={key}>
+        <p>{data.pinner.username}</p>
+        <img src={data.images["136x136"].url} />
+        <p>{ data.description}</p>
+        <p>{data.like_count}</p>
+      </div>
+  )})
+  
+  return (
+    <div className="board">
+      <h1>Pinterest</h1>
+      {imgJsx}
+            
+
+
+      {/* {pinData.map((data, key) => {
+        return (
+          <div key={key}>
+            <div>{data.images}</div>
+          </div>
+        )
+      })} */}
+      
+    </div>
+  )
+}
+
+
+
+
